47b302e46d6efa8f5ae610172ab8e8d53f0ba3a2
Haksiu <38468996+Haksiu@users.noreply.github.com>
2021-08-13 16:43:10 +0200

    Delete nadway_nickname_generator.py

--- nadway_nickname_generator.py
+++
@@ -1,8 +0,0 @@
-import random
-
-for i in range(1):
-    string = ''.join(random.sample('abcdefghijklmnoprstuwxyz',3 ))
-    way = 'wey'
-    result = string + way
-    print('Wygenerowana nazwa:')
-    print(result)
